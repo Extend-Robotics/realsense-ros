a681e8ee94bbd0b00ca6a87c64f7f5bc21dc9de4
Bartosz Meglicki <bmegli@op.pl>
2025-09-30 15:35:43 +0200

    script for librealsense2 2.51.1 udev rules fixup

--- scripts/libuvc_installation.sh
+++ scripts/libuvc_installation.sh
@@ -34,7 +34,6 @@ cd ./librealsense-2.51.1
 
 echo Install udev-rules
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/ 
-sudo cp config/99-realsense-d4xx-mipi-dfu.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && sudo udevadm trigger 
 mkdir build && cd build
 cmake ../ -DFORCE_LIBUVC=true -DBUILD_WITH_CUDA=true -DCMAKE_BUILD_TYPE=release
