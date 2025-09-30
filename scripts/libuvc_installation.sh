e356e21bc9214de79dd50c8b69380289a6d41bf6
Bartosz Meglicki <bmegli@op.pl>
2025-09-30 16:26:12 +0200

    restore udev rules for mipi-dfu
    - as we are now on 2.53.1 which has them

--- scripts/libuvc_installation.sh
+++ scripts/libuvc_installation.sh
@@ -36,6 +36,7 @@ cd ./librealsense-2.53.1
 
 echo Install udev-rules
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/ 
+sudo cp config/99-realsense-d4xx-mipi-dfu.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && sudo udevadm trigger 
 mkdir build && cd build
 cmake ../ -DFORCE_LIBUVC=true -DBUILD_WITH_CUDA=true -DCMAKE_BUILD_TYPE=release
